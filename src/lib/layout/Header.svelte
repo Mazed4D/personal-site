0176063af41d6369c7d5226086039d9c96ea0433
Milan Paunović <paunovic.milan@outlook.com>
2022-11-03 20:27:42 +0100

    init layout

---
+++ src/lib/layout/Header.svelte
@@ -0,0 +1,57 @@
+<header>
+	<h1>Milan Paunovic</h1>
+	<nav>
+		<ul>
+			<li>
+				<a href="/">Home</a>
+			</li>
+			<li>
+				<a href="/about">About</a>
+			</li>
+			<li>
+				<a href="/projects">Projects</a>
+			</li>
+			<li>
+				<a href="/contact">Contact</a>
+			</li>
+		</ul>
+	</nav>
+</header>
+
+<style lang="scss">
+	header {
+		display: inline-flex;
+		padding: 0 1rem;
+		color: #ffbe0b;
+	}
+	nav {
+		display: inline-flex;
+		align-items: center;
+		flex-grow: 1;
+	}
+	ul {
+		list-style: none;
+		flex-grow: 1;
+		align-items: stretch;
+		display: inline-flex;
+		align-items: center;
+		justify-content: flex-end;
+		gap: 2rem;
+		padding-right: 1rem;
+		a {
+			font-size: 1.2rem;
+			text-decoration: none;
+			color: #819595;
+			transition: all 250ms ease-in;
+			&:active,
+			&:hover {
+				color: #ffbe0b;
+				text-shadow: 0 0 15px #ffbe0b60, 0 0 #ffbe0b60;
+				-webkit-transition: all 0.5s;
+				-moz-transition: all 0.5s;
+				-o-transition: all 0.5s;
+				transition: all 0.5s;
+			}
+		}
+	}
+</style>
---
+++ src/routes/+layout.svelte
@@ -0,0 +1,33 @@
+<script>
+	import Header from '$lib/layout/Header.svelte';
+</script>
+
+<Header />
+<div class="main">
+	<slot />
+</div>
+<footer>
+	<p id="footer-text">
+		Built with <span id="coffee">coffee</span> & <span id="sveltekit">SvelteKit</span>
+	</p>
+</footer>
+
+<style lang="scss">
+	.main {
+		display: flex;
+		flex-direction: column;
+		flex-grow: 1;
+		padding: 1rem 2rem;
+	}
+	footer {
+		display: flex;
+		justify-content: center;
+		padding: 1rem;
+	}
+	#coffee {
+		color: #b2675e;
+	}
+	#sveltekit {
+		color: #ffbe0b;
+	}
+</style>
