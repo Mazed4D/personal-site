48444b0c770688cb34183d47fc2355896666818c
Milan Paunović <paunovic.milan@outlook.com>
2023-01-05 20:24:40 +0100

    changed description

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -4,10 +4,16 @@
 		<h1>Milan Paunovic - Frontend Developer</h1>
 	</section>
 	<section class="about-desc">
-		An independent, motivated and ambitious Frontend developer specializing in delivering
-		responsive, modern, and quick React/React-native-based applications with the best UX possible. I
-		always strive to improve my skills and embrace new technologies to create special user-oriented
-		software.
+		As a highly independent and ambitious Frontend developer, I am dedicated to delivering
+		responsive, modern, and user-friendly applications using React and React-Native. With a focus on
+		providing the best possible user experience, I am constantly seeking to improve my skills and
+		stay current with new technologies. I am open to learning new technologies and approaches to
+		software development in order to further enhance my skills and deliver exceptional results for
+		my clients. My passion for creating special user-oriented software is evident in my successful
+		track record of delivering high-quality projects, including the development of a
+		React-Native-based android application for hyper-local shopping and the creation of a platform
+		connecting businesses with remote workers using React (TypeScript) and Redux. I am eager to
+		bring my expertise and drive to a dynamic and forward-thinking team.
 	</section>
 </article>
 
