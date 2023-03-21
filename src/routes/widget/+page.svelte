b6d9338ef4dc6ae609b303b0c965b655af63190d
Milan Paunovic <milan.paunovic@visit.com>
2023-03-21 17:07:43 +0100

    changed the way widgets are displayed

--- src/routes/widget/+page.svelte
+++ src/routes/widget/+page.svelte
@@ -3,58 +3,63 @@
 </svelte:head>
 
 <script>
-	import { onMount } from "svelte";
+	import { onMount } from 'svelte';
+	const widgetLayouts = [
+		'Standard1Column',
+		'Standard1ColumnCompact',
+		'Standard2Columns',
+		'Standard2ColumnsCompact',
+		'Calendar2Columns',
+		'StandardWide',
+		'StandardWideCompact',
+		'Large2Columns',
+		'Large3Columns',
+		'Standard1ColumnSimplify',
+		'LinkOnly',
+		'Standard1ColumnPlate'
+	];
 
+	let selectedWidgetId = 0;
+
+	const handleCycleWidgetLayout = () => {
+		selectedWidgetId = selectedWidgetId + 1 > widgetLayouts.length ? 0 : selectedWidgetId + 1;
+	};
 
 	onMount(() => {
 		let confConfig = {};
-			confConfig.CustomHeader = 'Single prop widget';
-			// @ts-ignore
-			BV.CfWidget({
-				...confConfig,
-				WidgetContainerId : 'widgetContainer',
-				openOption: 'redirect',
-				promoCode: true,
-				corpCode: false,
-				agentCode: false,
-				loginLink: false,
-				type: "multi_property",
-				widgetLayout: 'StandardWide',
-				currency: 'SEK',
-				channelId: '35BEC48D-58A7-490F-A8BC-675201D8E810'
-			});
-			// 			// @ts-ignore
-			// 			BV.CfWidget({
-			// 	...confConfig,
-			// 	WidgetContainerId : 'widgetContainer2',
-			// 	openOption: 'redirect',
-			// 	promoCode: true,
-			// 	corpCode: false,
-			// 	agentCode: false,
-			// 	loginLink: false,
-			// 	type: "single_property",
-			// 	widgetLayout: 'StandardWide',
-			// 	currency: 'SEK',
-			// 	channelId: '35BEC48D-58A7-490F-A8BC-675201D8E810'
-			// });
-						// @ts-ignore
-						BV.CfWidget({
-				...confConfig,
-				WidgetContainerId : 'widgetContainer3',
-				openOption: 'redirect',
-				promoCode: true,
-				corpCode: false,
-				agentCode: false,
-				loginLink: false,
-				type: "multi_property",
-				widgetLayout: 'Standard1Column',
-				currency: 'SEK',
-				channelId: '35BEC48D-58A7-490F-A8BC-675201D8E810'
-			});
-	})
+		confConfig.CustomHeader = 'Single prop widget';
+		// @ts-ignore
+		BV.CfWidget({
+			...confConfig,
+			WidgetContainerId: 'widgetContainer',
+			openOption: 'redirect',
+			promoCode: true,
+			corpCode: false,
+			agentCode: false,
+			loginLink: false,
+			type: 'multi_property',
+			widgetLayout: widgetLayouts[5],
+			currency: 'SEK',
+			channelId: '35BEC48D-58A7-490F-A8BC-675201D8E810'
+		});
+		// @ts-ignore
+		BV.CfWidget({
+			...confConfig,
+			WidgetContainerId: 'widgetContainer3',
+			openOption: 'redirect',
+			promoCode: true,
+			corpCode: false,
+			agentCode: false,
+			loginLink: false,
+			type: 'multi_property',
+			widgetLayout: widgetLayouts[selectedWidgetId],
+			currency: 'SEK',
+			channelId: '35BEC48D-58A7-490F-A8BC-675201D8E810'
+		});
+	});
 </script>
 
-<div id="widgetContainer" style="width: 100%;" />
-<div id="widgetContainer2" style="width: 100%;" />
-<div id="widgetContainer3" style="width: 100%;" />
-
+<div>
+	<div id="widgetContainer"/>
+	<div id="widgetContainer3"/>
+</div>
