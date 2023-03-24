f946bcdb574859fa367d8327005bff07104099da
Milan Paunovic <milan.paunovic@visit.com>
2023-03-24 13:46:59 +0100

    fix

--- svelte.config.js
+++ svelte.config.js
@@ -1,13 +1,12 @@
 // import adapter from '@sveltejs/adapter-netlify';
 import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
-import importAssets from 'svelte-preprocess-import-assets'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://github.com/sveltejs/svelte-preprocess
 	// for more information about preprocessors
-	preprocess: [preprocess(), importAssets()],
+	preprocess: [preprocess()],
 
 	kit: {
 		adapter: adapter({
