aa31614d4d4d4f875210226f52346350889a7bf9
Milan Paunovic <milan.paunovic@visit.com>
2023-05-18 21:39:24 +0200

    table reservation widget test

---
+++ src/routes/widget/table/+page.svelte
@@ -0,0 +1,30 @@
+<script>
+	// @ts-nocheck
+	import { onMount } from 'svelte';
+
+	onMount(() => {
+		document.getElementById('table-reservation').addEventListener('click', () => {
+			const config = {};
+			config.widgetContainerId = 'widgetContainer';
+			config.open = true;
+			config.channelId = 'fe4e6594-def3-4878-8a3c-47a99b1d6137';
+			config.languageCulture = 'en-GB';
+			BV.TableReservationWidget({ ...config });
+		});
+	});
+</script>
+
+<svelte:head>
+	<script
+		type="text/javascript"
+		src="//online.bookvisit.com/Content/widget/table_reservation_widget.bundle.js"
+	></script>
+	<title>Table Reservation Script Test</title>
+</svelte:head>
+
+<div>
+	<div style="background-color: white;">
+		<div id="widgetContainer" />
+		<button id="table-reservation">Replace me!</button>
+	</div>
+</div>
